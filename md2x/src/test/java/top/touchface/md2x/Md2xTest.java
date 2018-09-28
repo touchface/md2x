aa49a74a2b48ce0c5bbdaec5f85db6f9d15188df
touchface <572165213@qq.com>
2018-09-28 18:13:09 +0800

    测试

--- md2x/src/test/java/top/touchface/md2x/Md2xTest.java
+++ md2x/src/test/java/top/touchface/md2x/Md2xTest.java
@@ -10,7 +10,8 @@ public class Md2xTest {
 	public void test1(){
 		Options options=new Options();
 		options.baseUrl="http://localhost:8080/files/";
-		System.out.println(Md2x.parseToHtml("![pic](image.jpg)",options));
+		options.headerPrefix="ABC";
+		System.out.println(Md2x.parseToHtml("## 哈哈",options));
 	}
 	
 }
